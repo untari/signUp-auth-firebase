da3b07c3bc899f149b8eaaff9903c07df2a33aa5
untari <sastraembun0306@gmail.com>
2021-11-16 20:35:41 +0700

    adding github ribbon

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -50,97 +50,103 @@ function AuthForm() {
   }, [user]);
 
   return (
-    <div className="auth-form-wrapper">
-      <Card className="auth-form-card">
-        <Card.Content>
-          {user ? (
-            renderLoggedIn()
-          ) : (
-            <Fragment>
-              <Card.Header className="auth-form-header">Auth Form</Card.Header>
-              <Menu compact secondary>
-                <Menu.Item
-                  name="Login"
-                  onClick={() => setIsLogin(true)}
-                  active={isLogin}
-                ></Menu.Item>
-                <Menu.Item
-                  name="Sign up"
-                  onClick={() => setIsLogin(false)}
-                  active={!isLogin}
-                ></Menu.Item>
-              </Menu>
-              {isLogin ? (
-                <Fragment>
-                  <Form>
-                    <Form.Field className="auth-form-fields">
-                      <label className="form-labels">Email</label>
-                      <input
-                        placeholder="Email Address"
-                        name="loginEmail"
-                        type="email"
-                        value={loginEmail || ""}
-                        onChange={(e) => setLoginEmail(e.target.value)}
-                      ></input>
-                    </Form.Field>
-                    <Form.Field className="auth-form-fields">
-                      <label className="form-labels">Password</label>
-                      <input
-                        placeholder="Password"
-                        name="loginPassword"
-                        type="password"
-                        value={loginPassword || ""}
-                        onChange={(e) => setLoginPassword(e.target.value)}
-                      ></input>
-                    </Form.Field>
-                    <Button
-                      onClick={() => authenticateUser(loginEmail, loginPassword, true)}
-                      className="auth-form-buttons"
-                      color="green"
-                    >
-                      Login
-                    </Button>
-                  </Form>
-                  <div className="google-login"></div>
-                </Fragment>
-              ) : (
-                <Fragment>
-                  <Form>
-                    <Form.Field className="auth-form-fields">
-                      <label className="form-labels">Email</label>
-                      <input
-                        placeholder="Email Address"
-                        name="signUpEmail"
-                        type="email"
-                        value={signupEmail || ""}
-                        onChange={(e) => setSignupEmail(e.target.value)}
-                      ></input>
-                    </Form.Field>
-                    <Form.Field className="auth-form-fields">
-                      <label className="form-labels">Password</label>
-                      <input
-                        placeholder="Password"
-                        name="signUpPassword"
-                        type="password"
-                        value={signupPassword || ""}
-                        onChange={(e) => setSignupPassword(e.target.value)}
-                      ></input>
-                    </Form.Field>
-                    <Button
-                      className="auth-form-buttons"
-                      color="teal"
-                      onClick={() => authenticateUser(signupEmail, signupPassword, false)}
-                    >
-                      Sign up
-                    </Button>
-                  </Form>
-                  <div className="google-login"></div>
-                </Fragment>
-              )}
-            </Fragment>
-          )}
-        </Card.Content>
-      </Card>
+    <div>
+      <div className="ribbon">
+        <a href="https://github.com/untari/signup-auth-firebase"><img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" id="github-ribbon"/>
+        </a>
+      </div>
+      <div className="auth-form-wrapper">
+        <Card className="auth-form-card">
+          <Card.Content>
+            {user ? (
+              renderLoggedIn()
+            ) : (
+              <Fragment>
+                <Card.Header className="auth-form-header">Auth Form</Card.Header>
+                <Menu compact secondary>
+                  <Menu.Item
+                    name="Login"
+                    onClick={() => setIsLogin(true)}
+                    active={isLogin}
+                  ></Menu.Item>
+                  <Menu.Item
+                    name="Sign up"
+                    onClick={() => setIsLogin(false)}
+                    active={!isLogin}
+                  ></Menu.Item>
+                </Menu>
+                {isLogin ? (
+                  <Fragment>
+                    <Form>
+                      <Form.Field className="auth-form-fields">
+                        <label className="form-labels">Email</label>
+                        <input
+                          placeholder="Email Address"
+                          name="loginEmail"
+                          type="email"
+                          value={loginEmail || ""}
+                          onChange={(e) => setLoginEmail(e.target.value)}
+                        ></input>
+                      </Form.Field>
+                      <Form.Field className="auth-form-fields">
+                        <label className="form-labels">Password</label>
+                        <input
+                          placeholder="Password"
+                          name="loginPassword"
+                          type="password"
+                          value={loginPassword || ""}
+                          onChange={(e) => setLoginPassword(e.target.value)}
+                        ></input>
+                      </Form.Field>
+                      <Button
+                        onClick={() => authenticateUser(loginEmail, loginPassword, true)}
+                        className="auth-form-buttons"
+                        color="green"
+                      >
+                        Login
+                      </Button>
+                    </Form>
+                    <div className="google-login"></div>
+                  </Fragment>
+                ) : (
+                  <Fragment>
+                    <Form>
+                      <Form.Field className="auth-form-fields">
+                        <label className="form-labels">Email</label>
+                        <input
+                          placeholder="Email Address"
+                          name="signUpEmail"
+                          type="email"
+                          value={signupEmail || ""}
+                          onChange={(e) => setSignupEmail(e.target.value)}
+                        ></input>
+                      </Form.Field>
+                      <Form.Field className="auth-form-fields">
+                        <label className="form-labels">Password</label>
+                        <input
+                          placeholder="Password"
+                          name="signUpPassword"
+                          type="password"
+                          value={signupPassword || ""}
+                          onChange={(e) => setSignupPassword(e.target.value)}
+                        ></input>
+                      </Form.Field>
+                      <Button
+                        className="auth-form-buttons"
+                        color="teal"
+                        onClick={() => authenticateUser(signupEmail, signupPassword, false)}
+                      >
+                        Sign up
+                      </Button>
+                    </Form>
+                    <div className="google-login"></div>
+                  </Fragment>
+                )}
+              </Fragment>
+            )}
+          </Card.Content>
+        </Card>
+      </div>
     </div>
   );
 }
